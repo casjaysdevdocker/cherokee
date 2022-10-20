50dfd9649ea3e82f9fd28b9e60bba1c1eb1b4f43
casjay <git-admin@casjaysdev.com>
2022-10-20 17:12:22 -0400

    🗃️ Committing everything that changed 🗃️

--- Dockerfile
+++ Dockerfile
@@ -1,28 +1,33 @@
 FROM casjaysdevdocker/python2:latest AS build
 
-ARG PORTS="80 9090"
+ARG ALPINE_VERSION="v3.16"
 
-ENV CFLAGS="-static"
+ARG DEFAULT_DATA_DIR="/usr/local/share/template-files/data" \
+  DEFAULT_CONF_DIR="/usr/local/share/template-files/config" \
+  DEFAULT_TEMPLATE_DIR="/usr/local/share/template-files/defaults"
+
+ARG PACK_LIST="bash autoconf musl-dev automake gettext git libtool \
+  openssl openssl-dev linux-headers rrdtool ffmpeg-dev geoip-dev php8-cgi"
+
+ENV LANG=en_US.utf8 \
+  ENV=ENV=~/.bashrc \
+  TZ="America/New_York" \
+  SHELL="/bin/sh" \
+  TERM="xterm-256color" \
+  TIMEZONE="${TZ:-$TIMEZONE}" \
+  HOSTNAME="casjaysdev-cherokee" \
+  CFLAGS="-static"
+
+RUN set -ex; \
+  rm -Rf "/etc/apk/repositories"; \
+  mkdir -p "${DEFAULT_DATA_DIR}" "${DEFAULT_CONF_DIR}" "${DEFAULT_TEMPLATE_DIR}"; \
+  echo "http://dl-cdn.alpinelinux.org/alpine/${ALPINE_VERSION}/main" >>"/etc/apk/repositories"; \
+  echo "http://dl-cdn.alpinelinux.org/alpine/${ALPINE_VERSION}/community" >>"/etc/apk/repositories"; \
+  if [ "${ALPINE_VERSION}" = "edge" ]; then echo "http://dl-cdn.alpinelinux.org/alpine/${ALPINE_VERSION}/testing" >>"/etc/apk/repositories" ; fi ; \
+  apk update --update-cache && apk add --no-cache ${PACK_LIST} && \
+  echo
 
 WORKDIR /tmp/build
-
-RUN apk -U upgrade && \
-  apk add --no-cache \
-  alpine-sdk \
-  autoconf \
-  musl-dev \
-  automake \
-  gettext \
-  git \
-  libtool \
-  openssl \
-  openssl-dev \
-  linux-headers \
-  rrdtool \
-  ffmpeg-dev \
-  geoip-dev \
-  php8-cgi
-
 RUN mkdir -p "/usr/local/share/template-files/config/defaults/cherokee" "/buildroot" && \
   cd /tmp/build && \
   git clone https://github.com/cherokee/webserver.git . && \
@@ -46,47 +51,79 @@ RUN mkdir -p "/usr/local/share/template-files/config/defaults/cherokee" "/buildr
   ln -sf /usr/local/share/cherokee/bin/* /usr/local/bin/ && \
   ln -sf /usr/local/share/cherokee/sbin/* /usr/local/bin/ && \
   cp -Rf "/etc/cherokee/." "/usr/local/share/template-files/config/defaults/cherokee/" && \
-  cp -Rf "/usr/local/." "/buildroot/" && \
-  rm -Rf /var/cache/apk/* /tmp/* /var/tmp/* /tmp/build /usr/src/*
+  cp -Rf "/usr/local/." "/buildroot/"
 
-FROM casjaysdevdocker/php:latest AS source
 
-RUN apk add --no-cache geoip rrdtool openssl && \
-  rm -Rf /var/cache/apk/* /tmp/* /var/tmp/*
+FROM casjaysdevdocker/php:latest AS php
 
+RUN apk add --no-cache geoip rrdtool openssl
+
+COPY ./rootfs/. /
 COPY --from=build /buildroot/. /usr/local/
-COPY ./bin/. /usr/local/bin/
-COPY ./data/. /usr/local/share/template-files/data/
-COPY ./config/. /usr/local/share/template-files/config/
+
+RUN echo 'Running cleanup' ; \
+  rm -Rf /usr/share/doc/* /usr/share/info/* /tmp/* /var/tmp/* ; \
+  rm -Rf /usr/local/bin/.gitkeep /usr/local/bin/.gitkeep /config /data /var/cache/apk/* ; \
+  rm -rf /lib/systemd/system/multi-user.target.wants/* ; \
+  rm -rf /etc/systemd/system/*.wants/* ; \
+  rm -rf /lib/systemd/system/local-fs.target.wants/* ; \
+  rm -rf /lib/systemd/system/sockets.target.wants/*udev* ; \
+  rm -rf /lib/systemd/system/sockets.target.wants/*initctl* ; \
+  rm -rf /lib/systemd/system/sysinit.target.wants/systemd-tmpfiles-setup* ; \
+  rm -rf /lib/systemd/system/systemd-update-utmp* ; \
+  if [ -d "/lib/systemd/system/sysinit.target.wants" ]; then cd "/lib/systemd/system/sysinit.target.wants" && rm $(ls | grep -v systemd-tmpfiles-setup) ; fi
 
 FROM scratch
-ARG BUILD_DATE="$(date +'%Y-%m-%d %H:%M')"
 
-LABEL \
-  org.label-schema.name="cherokee" \
-  description="Alpine based image with cherokee and php8." \
-  org.label-schema.url="https://hub.docker.com/r/casjaysdevdocker/cherokee" \
-  org.label-schema.vcs-url="https://github.com/casjaysdevdocker/cherokee" \
-  org.label-schema.build-date=$BUILD_DATE \
-  org.label-schema.version=$BUILD_DATE \
-  org.label-schema.vcs-ref=$BUILD_DATE \
-  org.label-schema.license="WTFPL" \
-  org.label-schema.vcs-type="Git" \
-  org.label-schema.schema-version="1.0" \
-  org.label-schema.vendor="CasjaysDev" \
-  maintainer="CasjaysDev <docker-admin@casjaysdev.com>"
+ARG \
+  SERVICE_PORT="80" \
+  EXPOSE_PORTS="80 9090" \
+  PHP_SERVER="cherokee" \
+  NODE_VERSION="system" \
+  NODE_MANAGER="system" \
+  BUILD_VERSION="latest" \
+  LICENSE="MIT" \
+  IMAGE_NAME="cherokee" \
+  BUILD_DATE="Thu Oct 20 04:49:27 PM EDT 2022" \
+  TIMEZONE="America/New_York"
 
-COPY --from=source /. /
+LABEL maintainer="CasjaysDev <docker-admin@casjaysdev.com>" \
+  org.opencontainers.image.vendor="CasjaysDev" \
+  org.opencontainers.image.authors="CasjaysDev" \
+  org.opencontainers.image.vcs-type="Git" \
+  org.opencontainers.image.name="${IMAGE_NAME}" \
+  org.opencontainers.image.base.name="${IMAGE_NAME}" \
+  org.opencontainers.image.license="${LICENSE}" \
+  org.opencontainers.image.vcs-ref="${BUILD_VERSION}" \
+  org.opencontainers.image.build-date="${BUILD_DATE}" \
+  org.opencontainers.image.version="${BUILD_VERSION}" \
+  org.opencontainers.image.schema-version="${BUILD_VERSION}" \
+  org.opencontainers.image.url="https://hub.docker.com/r/casjaysdevdocker/${IMAGE_NAME}" \
+  org.opencontainers.image.vcs-url="https://github.com/casjaysdevdocker/${IMAGE_NAME}" \
+  org.opencontainers.image.url.source="https://github.com/casjaysdevdocker/${IMAGE_NAME}" \
+  org.opencontainers.image.documentation="https://hub.docker.com/r/casjaysdevdocker/${IMAGE_NAME}" \
+  org.opencontainers.image.description="Containerized version of ${IMAGE_NAME}"
 
-ENV PHP_SERVER=cherokee
+ENV LANG=en_US.utf8 \
+  ENV=~/.bashrc \
+  SHELL="/bin/bash" \
+  PORT="${SERVICE_PORT}" \
+  TERM="xterm-256color" \
+  PHP_SERVER="${PHP_SERVER}" \
+  CONTAINER_NAME="${IMAGE_NAME}" \
+  TZ="${TZ:-America/New_York}" \
+  TIMEZONE="${TZ:-$TIMEZONE}" \
+  HOSTNAME="casjaysdev-${IMAGE_NAME}"
 
+COPY --from=php /. /
+
+USER root
 WORKDIR /data/htdocs/www
 
-EXPOSE $PORTS
+VOLUME [ "/config","/data" ]
 
-VOLUME [ "/data", "/config" ]
+EXPOSE $EXPOSE_PORTS
 
-ENTRYPOINT [ "tini", "--" ]
-HEALTHCHECK --interval=15s --timeout=3s CMD [ "/usr/local/bin/entrypoint-cherokee.sh" "healthcheck" ]
-CMD ["/usr/local/bin/entrypoint-cherokee.sh"]
-
+#CMD [ "" ]
+ENTRYPOINT [ "tini", "-p", "SIGTERM", "--", "/usr/local/bin/entrypoint.sh" ]
+HEALTHCHECK --start-period=1m --interval=2m --timeout=3s CMD [ "/usr/local/bin/entrypoint.sh", "healthcheck" ]
---
+++ rootfs/usr/local/bin/start-cherokee.sh
@@ -0,0 +1,166 @@
+#!/usr/bin/env bash
+# shellcheck shell=bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version           :  202210201649-git
+# @@Author           :  Jason Hempstead
+# @@Contact          :  jason@casjaysdev.com
+# @@License          :  LICENSE.md
+# @@ReadME           :  start-cherokee.sh --help
+# @@Copyright        :  Copyright: (c) 2022 Jason Hempstead, Casjays Developments
+# @@Created          :  Thursday, Oct 20, 2022 16:49 EDT
+# @@File             :  start-cherokee.sh
+# @@Description      :  script to start cherokee
+# @@Changelog        :  New script
+# @@TODO             :  Better documentation
+# @@Other            :
+# @@Resource         :
+# @@Terminal App     :  no
+# @@sudo/root        :  no
+# @@Template         :  other/start-service
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set functions
+__pcheck() { [ -n "$(which pgrep 2>/dev/null)" ] && pgrep -x "$1" || return 1; }
+__find() { find "$1" -mindepth 1 -type ${2:-f,d} 2>/dev/null | grep '^' || return 10; }
+__curl() { curl -q -LSsf -o /dev/null -s -w "200" "$@" 2>/dev/null || return 10; }
+__pgrep() { __pcheck "$1" || ps aux 2>/dev/null | grep -Fw " $1" | grep -qv ' grep' || return 10; }
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__certbot() {
+  [ -n "$DOMANNAME" ] && [ -n "$CERT_BOT_MAIL" ] || { echo "The variables DOMANNAME and CERT_BOT_MAIL are set" && exit 1; }
+  [ "$SSL_CERT_BOT" = "true" ] && type -P certbot &>/dev/null || { export SSL_CERT_BOT="" && return 10; }
+  certbot $1 --agree-tos -m $CERT_BOT_MAIL certonly --webroot -w "${WWW_ROOT_DIR:-/data/htdocs/www}" -d $DOMAINNAME -d $DOMAINNAME \
+    --put-all-related-files-into "$SSL_DIR" -key-path "$SSL_KEY" -fullchain-path "$SSL_CERT"
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+__heath_check() {
+  status=0 health="Good"
+  __pgrep ${1:-} || status=$((status + 1))
+  #__curl "http://localhost:$HTTP_PORT/server-health" || status=$((status + 1))
+  [ "$status" -eq 0 ] || health="Errors reported see docker logs --follow $CONTAINER_NAME"
+  echo "$(uname -s) $(uname -m) is running and the health is: $health"
+  return ${status:-$?}
+}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set variables
+DISPLAY="${DISPLAY:-}"
+LANG="${LANG:-C.UTF-8}"
+DOMAINNAME="${DOMAINNAME:-}"
+TZ="${TZ:-America/New_York}"
+HTTP_PORT="${HTTP_PORT:-80}"
+HTTPS_PORT="${HTTPS_PORT:-}"
+SERVICE_PORT="${SERVICE_PORT:-$HTTP_PORT}"
+SERVICE_NAME="${CONTAINER_NAME:-}"
+HOSTNAME="${HOSTNAME:-casjaysdev-cherokee}"
+HOSTADMIN="${HOSTADMIN:-root@${DOMAINNAME:-$HOSTNAME}}"
+SSL_CERT_BOT="${SSL_CERT_BOT:-false}"
+SSL_ENABLED="${SSL_ENABLED:-false}"
+SSL_DIR="${SSL_DIR:-/config/ssl}"
+SSL_CA="${SSL_CA:-$SSL_DIR/ca.crt}"
+SSL_KEY="${SSL_KEY:-$SSL_DIR/server.key}"
+SSL_CERT="${SSL_CERT:-$SSL_DIR/server.crt}"
+SSL_CONTAINER_DIR="${SSL_CONTAINER_DIR:-/etc/ssl/CA}"
+WWW_ROOT_DIR="${WWW_ROOT_DIR:-/data/htdocs}"
+LOCAL_BIN_DIR="${LOCAL_BIN_DIR:-/usr/local/bin}"
+DATA_DIR_INITIALIZED="${DATA_DIR_INITIALIZED:-}"
+CONFIG_DIR_INITIALIZED="${CONFIG_DIR_INITIALIZED:-}"
+DEFAULT_DATA_DIR="${DEFAULT_DATA_DIR:-/usr/local/share/template-files/data}"
+DEFAULT_CONF_DIR="${DEFAULT_CONF_DIR:-/usr/local/share/template-files/config}"
+DEFAULT_TEMPLATE_DIR="${DEFAULT_TEMPLATE_DIR:-/usr/local/share/template-files/defaults}"
+CONTAINER_IP_ADDRESS="$(ip a 2>/dev/null | grep 'inet' | grep -v '127.0.0.1' | awk '{print $2}' | sed 's|/.*||g')"
+[ -n "$HTTP_PORT" ] || [ -n "$HTTPS_PORT" ] || HTTP_PORT="$SERVICE_PORT"
+[ "$HTTPS_PORT" = "443" ] && HTTP_PORT="$HTTPS_PORT" && SSL_ENABLED="true"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Overwrite variables
+#SERVICE_PORT=""
+SERVICE_NAME="cherokee"
+SERVICE_COMMAND="cherokee -C /etc/cherokee/cherokee.conf"
+export exec_message="Starting $SERVICE_NAME on $CONTAINER_IP_ADDRESS:$SERVICE_PORT"
+export PATH="/usr/local/share/cherokee/bin:/usr/local/share/cherokee/sbin:$PATH"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Pre copy commands
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Check if this is a new container
+[ -z "$DATA_DIR_INITIALIZED" ] && [ -f "/data/.docker_has_run" ] && DATA_DIR_INITIALIZED="true"
+[ -z "$CONFIG_DIR_INITIALIZED" ] && [ -f "/config/.docker_has_run" ] && CONFIG_DIR_INITIALIZED="true"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Create default config
+if [ "$CONFIG_DIR_INITIALIZED" = "false" ] && [ -n "$DEFAULT_TEMPLATE_DIR" ]; then
+  [ -d "/config" ] && cp -Rf "$DEFAULT_TEMPLATE_DIR/." "/config/" 2>/dev/null
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Copy custom config files
+if [ "$CONFIG_DIR_INITIALIZED" = "false" ] && [ -n "$DEFAULT_CONF_DIR" ]; then
+  [ -d "/config" ] && cp -Rf "$DEFAULT_CONF_DIR/." "/config/" 2>/dev/null
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Copy custom data files
+if [ "$DATA_DIR_INITIALIZED" = "false" ] && [ -n "$DEFAULT_DATA_DIR" ]; then
+  [ -d "/data" ] && cp -Rf "$DEFAULT_DATA_DIR/." "/data/" 2>/dev/null
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Copy html files
+if [ "$DATA_DIR_INITIALIZED" = "false" ] && [ -d "$DEFAULT_DATA_DIR/data/htdocs" ]; then
+  [ -d "/data" ] && cp -Rf "$DEFAULT_DATA_DIR/data/htdocs/." "$WWW_ROOT_DIR/" 2>/dev/null
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Post copy commands
+[ -d "/data/cherokee/graphs" ] || mkdir -p "/data/cherokee/graphs"
+[ -d "/config/cherokee/themes" ] && cp -Rf "/config/cherokee/themes/." "/usr/local/share/cherokee/share/cherokee/themes/"
+[ -d "/usr/local/share/cherokee/icons" ] && [ ! -d "/config/cherokee/icons" ] && mkdir -p "/config/cherokee/icons" && cp -Rf "/usr/local/share/cherokee/icons/." "/config/cherokee/icons/"
+[ -d "/usr/local/share/cherokee/themes" ] && [ ! -d "/config/cherokee/themes" ] && mkdir -p "/config/cherokee/themes" && cp -Rf "/usr/local/share/cherokee/themes/." "/config/cherokee/themes/"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Initialized
+[ -d "/data" ] && touch "/data/.docker_has_run"
+[ -d "/config" ] && touch "/config/.docker_has_run"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# APP Variables overrides
+[ -f "/root/env.sh" ] && . "/root/env.sh"
+[ -f "/config/env.sh" ] && "/config/env.sh"
+[ -f "/config/.env.sh" ] && . "/config/.env.sh"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Actions based on env
+chmod -Rfv 664 "/data/cherokee"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# begin main app
+case "$1" in
+healthcheck)
+  shift 1
+  __heath_check "${SERVICE_NAME:-bash}"
+  exit $?
+  ;;
+
+certbot)
+  shift 1
+  SSL_CERT_BOT="true"
+  if [ "$1" = "create" ]; then
+    shift 1
+    __certbot
+  elif [ "$1" = "renew" ]; then
+    shift 1
+    __certbot "renew certonly --force-renew"
+  else
+    __exec_command "certbot" "$@"
+  fi
+  ;;
+
+*)
+  if __pgrep "$SERVICE_NAME" && [ ! -f "/tmp/$SERVICE_NAME.pid" ]; then
+    echo "$SERVICE_NAME is running"
+  else
+    touch "/tmp/$SERVICE_NAME.pid"
+    echo "Starting cherokee-admin on port 9090"
+    cherokee-admin -b -p 9090 -C /etc/cherokee/cherokee.conf &
+    __exec_command "$SERVICE_COMMAND" || rm -Rf "/tmp/$SERVICE_NAME.pid"
+  fi
+  ;;
+esac
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Set exit code
+exitCode="${exitCode:-$?}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# End application
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# lets exit with code
+exit ${exitCode:-$?}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# end
